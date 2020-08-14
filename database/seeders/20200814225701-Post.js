ef68fc030ff7981212408080e1a1f995e8d36e0f
caio_ingber <caioingber@gmail.com>
2020-08-14 19:22:43 -0400

    seeded initial data

--- database/seeders/20200814225701-Post.js
+++ database/seeders/20200814225701-Post.js
@@ -1,24 +1,30 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert("Posts", [
+      {
+        title: "Cute pupper",
+        userId: 1,
+        image:
+          "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg",
+        caption: "cute lil fella",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "Mr Meow",
+        userId: 2,
+        image:
+          "https://static.toiimg.com/photo/msid-67586673/67586673.jpg?resizemode=4&width=400",
+        caption: "can I has?",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+    await queryInterface.bulkDelete("Posts", null, {});
+  },
 };
--- database/seeders/20200814225707-Comment.js
+++ database/seeders/20200814225707-Comment.js
@@ -2,15 +2,25 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert(Comments);
+    await queryInterface.bulkInsert("Comments", [
+      {
+        userId: 2,
+        postId: 1,
+        content: "Well hello there, Mr. pup!",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        userId: 1,
+        postId: 2,
+        content: "Meow Meow, what a cutey!",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("Comments", null, {});
   },
 };
